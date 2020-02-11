4cc2f24d49f49f2369c13a592db21d28ab5042c7
Andrew Triola <andrewtriola@gmail.com>
2020-02-11 15:58:22 -0500

    updated the df with inches in height column

--- rtr_1.py
+++ rtr_1.py
@@ -24,3 +24,18 @@ df = df.rename(columns={'bust size': 'bust_size', 'rented for': 'rented_for', 'b
 df['bust_size'] = df['bust_size'].str.split('(\D+)')
 df['cup_size'] = df.bust_size.str[1]
 df['bust_size'] = df.bust_size.str[0]
+
+### change height from feet' inches" to inches (float)
+condition = df['height'] != 'nan'
+df = df[condition]
+
+def parse_ht(ht):
+    # format: 7' 0.0"
+    ht_ = ht.split("' ")
+    ft_ = float(ht_[0])
+    in_ = float(ht_[1].replace("\"",""))
+    return (12*ft_) + in_
+
+df['height'] = df['height'].astype(str)
+df['height'] = df['height'].str.split('"')
+df['height'] = df['height'].apply(lambda x:parse_ht(x))
