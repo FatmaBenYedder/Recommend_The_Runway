5bfc0cba0f59bf32ec6b6adb630eb4972392490e
Chad Balloo <chadwick.balloo@gmail.com>
2020-02-12 06:35:42 -0500

    britneys work

--- rtr_1.py
+++ rtr_1.py
@@ -24,3 +24,16 @@ df = df.rename(columns={'bust size': 'bust_size', 'rented for': 'rented_for', 'b
 df['bust_size'] = df['bust_size'].str.split('(\D+)')
 df['cup_size'] = df.bust_size.str[1]
 df['bust_size'] = df.bust_size.str[0]
+
+##regroup categories (reduce from 68 to 7)
+recat1 = df.replace(['dress', 'sheath', 'shirtdress', 'shift', 'ballgown', 'frock', 'kaftan', 'caftan', 'gown', 'print'], 'dresses')
+recat2 = recat1.replace(['romper', 'jumpsuit', 'overalls', 'combo', 'suit'], 'jumpsuits')
+recat3 = recat2.replace(['jogger', 'trousers', 'tight', 'jeans', 'sweatpants', 'leggings', 'pants', 'culottes', 'legging', 'pant', 'culotte', 'trouser'], 'pants')
+recat4 = recat3.replace(['sweater', 'duster', 'cardigan', 'sweatshirt', 'pullover', 'turtleneck', 'hoodie', 'sweatershirt'], 'sweaters')
+recat5 = recat4.replace(['jacket', 'coat', 'trench', 'cape', 'bomber', 'blazer', 'vest', 'poncho', 'down', 'parka', 'peacoat', 'overcoat'], 'outerwear')
+recat6 = recat5.replace(['top,', 'shirt', 'blouse', 'tank', 'tunic', 'knit', 'tee', 'henley', 'blouson', 't-shirt', 'kimono', 'cami', 'crewneck', 'buttondown', 'for'], 'tops')
+recat7 = recat6.replace(['mini', 'skirt', 'maxi','midi', 'skirts', 'skort'], 'skirts')
+recat8 = recat7.replace(['top'], 'tops')
+
+##rename recategorized df variable
+df = recat8
